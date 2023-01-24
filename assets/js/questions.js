0d1ae125432613163c484cd24b161d307dda7609
Your Name <you@example.com>
2023-01-24 16:17:44 +0000

    saving my work

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        "title": "Common Data Types DON'T include:",
+        "choices": ["string", "booleans", "alerts", "numbers"],
+        "answer": "alerts",
+    },
+    {
+        "title": "Conditional keywords are:",
+        "choices": ["if", "for", "while", "nfor each"],
+        "answer": "if",
+    },
+    {
+        "title": "What is JavaScript?",
+        "choices": ["a scripting language used to make the website interactive", "an assembly language used to make the website interactive", "a compiled language used to make the website interactive", "None of the mentioned"],
+        "answer": "a scripting language used to make the website interactive"
+    }
+]
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,9 +1,28 @@
 function printHighScores(){
+    let highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    highScores.sort(function(a,b){
+        return b.score-a.score;
+
+    })
+
+    highScores.forEach(function(score) {
+        let li = document.createElement("li");
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById("highscores");
+        ol.appendChild(li);
+    });
 
 }
 
 function clearHighScores(){
+    localStorage.removeItem("highscores");
+    window.location.reload();
 
 }
 
-clearButton = document.getElementById("clear");
+let clearButton = document.getElementById("clear");
+clearButton.addEventListner("click", clearHighScores);
+
+printHighScores();
